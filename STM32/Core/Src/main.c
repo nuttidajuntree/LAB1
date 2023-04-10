1d68e19b7976ff46959f0a04971df792c2afb619
nuttidajuntree <122718174+nuttidajuntree@users.noreply.github.com>
2023-04-10 09:56:03 +0700

    final

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -42,6 +42,33 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+struct PortPin
+{
+	GPIO_TypeDef* PORT;
+	uint16_t PIN;
+};
+
+struct PortPin R[4] =
+{
+		{GPIOA, GPIO_PIN_10},
+		{GPIOB, GPIO_PIN_3},
+		{GPIOB, GPIO_PIN_5},
+		{GPIOB, GPIO_PIN_4}
+};
+
+struct PortPin L[4] =
+{
+		{GPIOA, GPIO_PIN_9},
+		{GPIOC, GPIO_PIN_7},
+		{GPIOB, GPIO_PIN_6},
+		{GPIOA, GPIO_PIN_7}
+};
+uint16_t a = 0;
+//uint16_t ButtonMatrix = 0;
+uint16_t ButtonMatrix_Last = 0;
+uint16_t N = 0;
+uint16_t IDinput[11] = {0};
+uint16_t count = 0;
 
 /* USER CODE END PV */
 
@@ -50,7 +77,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+uint16_t ReadMatrixButton_1Row();
+void checkinput(uint16_t ButtonMatrix);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -98,6 +126,12 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  static uint32_t timestamp=0;
+	  if(HAL_GetTick()>=timestamp){
+		  timestamp=HAL_GetTick()+50;
+		  a = ReadMatrixButton_1Row();
+		  checkinput(a);
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -199,6 +233,12 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(R1_GPIO_Port, R1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, R2_Pin|R4_Pin|R3_Pin, GPIO_PIN_SET);
+
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
@@ -212,10 +252,112 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : L4_Pin L1_Pin */
+  GPIO_InitStruct.Pin = L4_Pin|L1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : L2_Pin */
+  GPIO_InitStruct.Pin = L2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(L2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : R1_Pin */
+  GPIO_InitStruct.Pin = R1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(R1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : R2_Pin R4_Pin R3_Pin */
+  GPIO_InitStruct.Pin = R2_Pin|R4_Pin|R3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : L3_Pin */
+  GPIO_InitStruct.Pin = L3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(L3_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
+uint16_t ReadMatrixButton_1Row()
+{
+	//
+	static uint8_t X = 0;
+	static uint16_t ButtonMatrix = 0;
+	//READ L1-L4
+	register int i;
+	for(i=0;i<4;i++)
+	{
+		if(HAL_GPIO_ReadPin(L[i].PORT, L[i].PIN))
+		{
+			ButtonMatrix &= ~(1<<X*4+i);
+		}
+		else
+		{
+			ButtonMatrix |= 1<<(X*4+i);
+		}
+	}
+	//SET RX
+	HAL_GPIO_WritePin(R[X].PORT, R[X].PIN, 1);
+	//RESET RX+1%4
+	HAL_GPIO_WritePin(R[(X+1)%4].PORT, R[(X+1)%4].PIN, 0);
+	X++;
+	X%=4;
+	return ButtonMatrix;
+}
 
+void checkinput(uint16_t ButtonMatrix)
+{
+	if(ButtonMatrix_Last == 0 && ButtonMatrix != 0){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+		if(ButtonMatrix == 32768){			// OK
+			if(IDinput[0] == 16 && IDinput[1] == 4 && IDinput[2] == 8 && IDinput[3] == 8 && IDinput[4] == 8 && IDinput[5] == 32 && IDinput[6] == 8 && IDinput[7] == 2 && IDinput[8] == 1024 && IDinput[9] == 2 && IDinput[10] == 512)
+			{		// 64340500018
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+			}
+		}
+		else if(ButtonMatrix == 4096){		// CLEAR
+			register int i;
+			for(i=11;i>=1;i--)
+			{
+				IDinput[i-1]=0;
+			}
+			count = 0;
+		}
+		else if(ButtonMatrix == 8192){		// BACKSPACE
+			IDinput[0]=0;
+			N = 1;
+			count--;
+		}
+		else if(N == 1){
+			IDinput[0] = ButtonMatrix;
+			N = 0;
+			count++;
+		}
+		else if(ButtonMatrix == 128 || ButtonMatrix == 2048 || ButtonMatrix == 16384)
+		{
+			count = count;
+		}
+		else if(count < 11){				// NUMBER
+			register int j;
+			for(j=10;j>=1;j--)
+			{
+				IDinput[j]=IDinput[j-1];
+			}
+			IDinput[0] = ButtonMatrix;
+			count++;
+		}
+	}
+	ButtonMatrix_Last = ButtonMatrix;
+}
 /* USER CODE END 4 */
 
 /**
